30a2ab5860a3f0b489ea2082bcb2854a6d958ee1
MarcusMo614 <53132292+MarcusMo614@users.noreply.github.com>
2020-01-17 20:36:34 -0500

    New animations + About Section
    Navbar now higlights depedning on what part of the page you are on.
    More work to be done on about / contact.

--- js/index.js
+++ js/index.js
@@ -6,15 +6,18 @@ $( document ).ready(function() {
 //     scrollTop: $("#showcase").offset().top
 // }, 1000);
 
-$('a[href^="#showcase"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
+// $('a[href^="#showcase"]').on('click', function(event) {
+//     var target = $(this.getAttribute('href'));
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({
+//             scrollTop: target.offset().top
+//         }, 1000);
+//     }
+// });
+
+
+
 
 $('a[href^="#top"]').on('click', function(event) {
     var target = $(this.getAttribute('href'));
@@ -26,14 +29,6 @@ $('a[href^="#top"]').on('click', function(event) {
     }
 });
 
-$('.navButton').on('click', function(){
-    $(this).toggleClass('active');
-
-    if ($(this).hasClass('active')){
-        $(this).removeClass('hvr-bob:hover');
-    }
-
-});
 
 
 // var body = document.body,timer;
@@ -49,3 +44,52 @@ $('.navButton').on('click', function(){
 
 
 ScrollReveal().reveal('.hello3', {delay:600});
+
+ScrollReveal().reveal('.scrollAlert p', {delay:1000});
+
+
+
+var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 800);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
